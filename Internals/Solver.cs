5a38436fca7d79583fa7be9eab878ef1e0f90d83
RoelerCoaster <RoelerCoaster@users.noreply.github.com>
2023-11-30 21:20:50 +0100

    Improve console output

--- Internals/Solver.cs
+++ Internals/Solver.cs
@@ -48,6 +48,8 @@ internal class Solver
         var table = new Table()
             .AddColumns("Part", "Solution", "Time");
 
+        var exceptions = new List<Exception>();
+
         await AnsiConsole.Live(table)
             .StartAsync(async ctx =>
             {
@@ -56,6 +58,7 @@ internal class Solver
                     PartToRun.Part1,
                     PartToRun.Part2
                 };
+
 
                 foreach (var part in parts)
                 {
@@ -73,8 +76,23 @@ internal class Solver
 
                     WriteSolution(table, solution, partNumber);
                     ctx.Refresh();
+
+                    if (solution.Exception != null)
+                    {
+                        exceptions.Add(solution.Exception);
+                    }
                 }
             });
+
+        if (exceptions.Any())
+        {
+            AnsiConsole.Write(new Rule("[red]Exceptions[/]"));
+            exceptions.ForEach(ex =>
+            {
+                AnsiConsole.WriteException(ex);
+                AnsiConsole.WriteLine();
+            });
+        }
     }
 
     private void WriteSolution(Table table, PartSolution solution, int partNumber)
@@ -88,7 +106,6 @@ internal class Solver
                 break;
             case SolutionType.Error:
                 table.UpdateCell(row, 1, "[red]ERROR[/]");
-                AnsiConsole.WriteException(solution.Exception!);
                 break;
             case SolutionType.Valid:
                 table.UpdateCell(row, 1, new Text(solution.Answer!, Style.Parse("green")));
