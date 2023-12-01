794f9434fbe8c465d052ab4dec0f3cd2e715e501
RoelerCoaster <RoelerCoaster@users.noreply.github.com>
2023-12-01 22:56:08 +0100

    Day 1

--- Solutions/Day01/Day01.cs
+++ Solutions/Day01/Day01.cs
@@ -1,4 +1,6 @@
+using MoreLinq;
 using RoelerCoaster.AdventOfCode.Year2023.Internals.Model;
+using RoelerCoaster.AdventOfCode.Year2023.Util;
 
 namespace RoelerCoaster.AdventOfCode.Year2023.Solutions.Day01;
 
@@ -12,11 +14,55 @@ internal class Day01 : DayBase
 
     protected override async Task<string> SolvePart1(string input)
     {
-        throw new NotImplementedException();
+        var calibrationValues = input
+            .Lines()
+            .Select(line => line.Where(char.IsAsciiDigit).ToList())
+            .Select(digits => int.Parse(digits[0].ToString() + digits[^1]))
+            .ToList();
+
+        return calibrationValues.Sum().ToString();
     }
 
     protected override async Task<string> SolvePart2(string input)
     {
-        throw new NotImplementedException();
+        var digitMap = new Dictionary<string, string>
+        {
+           { "one", "1" },
+           { "two", "2" },
+           { "three", "3" },
+           { "four",  "4" },
+           { "five", "5" },
+           { "six", "6" },
+           { "seven", "7" },
+           { "eight", "8" },
+           { "nine", "9" }
+        };
+
+
+        var calibrationValues = input
+            .Lines()
+            .Select(line =>
+            {
+                var all = digitMap.Keys.Concat(digitMap.Values).ToList();
+
+                var firstMatches = all.ToDictionary(d => d, d => line.IndexOf(d));
+                var lastMatches = all.ToDictionary(d => d, d => line.LastIndexOf(d));
+
+
+                var firstDigit = firstMatches.Where(kp => kp.Value >= 0)
+                    .MinBy(kp => kp.Value)
+                    .Key;
+
+                var lastDigit = lastMatches.Where(kp => kp.Value >= 0)
+                    .MaxBy(kp => kp.Value)
+                    .Key;
+
+                return (firstDigit.Length == 1 ? firstDigit : digitMap[firstDigit]) +
+                        (lastDigit.Length == 1 ? lastDigit : digitMap[lastDigit]);
+            })
+            .Select(int.Parse)
+            .ToList();
+
+        return calibrationValues.Sum().ToString();
     }
 }
