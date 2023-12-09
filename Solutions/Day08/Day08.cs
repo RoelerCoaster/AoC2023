3629584d4246980455b9d6ee2e07f98a03c3ce6e
RoelerCoaster <RoelerCoaster@users.noreply.github.com>
2023-12-09 01:07:27 +0100

    Day 8

--- Solutions/Day08/Day08.cs
+++ Solutions/Day08/Day08.cs
@@ -1,6 +1,10 @@
 using RoelerCoaster.AdventOfCode.Year2023.Internals.Model;
+using RoelerCoaster.AdventOfCode.Year2023.Util;
+using System.Text.RegularExpressions;
 
 namespace RoelerCoaster.AdventOfCode.Year2023.Solutions.Day08;
+
+internal record Node(string Label, string Left, string Right);
 
 internal class Day08 : DayBase
 {
@@ -12,11 +16,78 @@ internal class Day08 : DayBase
 
     protected override async Task<string> SolvePart1(string input)
     {
-        throw new NotImplementedException();
+        var sections = input.Sections();
+
+        var instructions = sections[0];
+
+        var nodeMap = sections[1].Lines().Select(ParseNode).ToDictionary(n => n.Label);
+
+        var currentNodeLabel = "AAA";
+        var numberOfSteps = 0;
+
+        do
+        {
+            var node = nodeMap[currentNodeLabel];
+
+            currentNodeLabel = instructions[numberOfSteps % instructions.Length] switch
+            {
+                'L' => node.Left,
+                'R' => node.Right,
+                _ => throw new InvalidOperationException("Invalid instruction")
+            };
+
+            numberOfSteps++;
+        } while (currentNodeLabel != "ZZZ");
+
+        return numberOfSteps.ToString();
     }
 
     protected override async Task<string> SolvePart2(string input)
     {
-        throw new NotImplementedException();
+        var sections = input.Sections();
+
+        var instructions = sections[0];
+
+        var nodeMap = sections[1].Lines().Select(ParseNode).ToDictionary(n => n.Label);
+
+        var currentNodeLabels = nodeMap.Keys.Where(l => l.EndsWith('A')).ToList();
+
+        var stepsUntilFirstZ = currentNodeLabels.Select(c =>
+            {
+                var numberOfSteps = 0;
+
+                var currentNodeLabel = c;
+                do
+                {
+                    var node = nodeMap[currentNodeLabel];
+
+                    currentNodeLabel = instructions[numberOfSteps % instructions.Length] switch
+                    {
+                        'L' => node.Left,
+                        'R' => node.Right,
+                        _ => throw new InvalidOperationException("Invalid instruction")
+                    };
+
+                    numberOfSteps++;
+                } while (!currentNodeLabel.EndsWith('Z'));
+
+                return (long)numberOfSteps;
+            })
+            .ToList();
+
+
+        return LCM(stepsUntilFirstZ).ToString();
+    }
+
+    private Node ParseNode(string line)
+    {
+        var matches = Regex.Matches(line, "\\w{3}");
+
+        return new Node(matches[0].Value, matches[1].Value, matches[2].Value);
+    }
+
+    private long LCM(List<long> numbers)
+    {
+        return numbers.Aggregate(MathUtil.LCM);
     }
 }
---
+++ Util/MathUtil.cs
@@ -0,0 +1,20 @@
+﻿using System.Numerics;
+
+namespace RoelerCoaster.AdventOfCode.Year2023.Util;
+internal static class MathUtil
+{
+    public static T GCD<T>(T a, T b) where T : IBinaryNumber<T>
+    {
+        while (b != T.Zero)
+        {
+            (a, b) = (b, a % b);
+        };
+
+        return a;
+    }
+
+    public static T LCM<T>(T a, T b) where T : IBinaryNumber<T>
+    {
+        return T.CopySign(a * b, T.One) / GCD(a, b);
+    }
+}
