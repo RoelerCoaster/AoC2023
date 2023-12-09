a7c94bfba1a183312c8663611def6bdd04804e15
RoelerCoaster <RoelerCoaster@users.noreply.github.com>
2023-12-09 21:56:22 +0100

    Day 9

--- Solutions/Day09/Day09.cs
+++ Solutions/Day09/Day09.cs
@@ -1,4 +1,6 @@
+using MoreLinq;
 using RoelerCoaster.AdventOfCode.Year2023.Internals.Model;
+using RoelerCoaster.AdventOfCode.Year2023.Util;
 
 namespace RoelerCoaster.AdventOfCode.Year2023.Solutions.Day09;
 
@@ -12,11 +14,51 @@ internal class Day09 : DayBase
 
     protected override async Task<string> SolvePart1(string input)
     {
-        throw new NotImplementedException();
+        var predictions = input
+            .Lines()
+            .Select(l => l.NumbersBySeparator<int>(" "))
+            .Select(PredictNext)
+            .ToList();
+
+        return predictions.Sum().ToString();
     }
 
     protected override async Task<string> SolvePart2(string input)
     {
-        throw new NotImplementedException();
+        var predictions = input
+                    .Lines()
+                    .Select(l => l.NumbersBySeparator<int>(" "))
+                    .Select(PredictPrevious)
+                    .ToList();
+
+        return predictions.Sum().ToString();
+    }
+
+    private int PredictNext(int[] history)
+    {
+        if (history.All(x => x == 0))
+        {
+            return 0;
+        }
+
+        var differences = history.Pairwise((a, b) => b - a).ToArray();
+
+        var prediction = PredictNext(differences);
+
+        return prediction + history[^1];
+    }
+
+    private int PredictPrevious(int[] history)
+    {
+        if (history.All(x => x == 0))
+        {
+            return 0;
+        }
+
+        var differences = history.Pairwise((a, b) => b - a).ToArray();
+
+        var prediction = PredictPrevious(differences);
+
+        return history[0] - prediction;
     }
 }
