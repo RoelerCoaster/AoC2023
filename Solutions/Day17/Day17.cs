d3f69d8a4b9412a73a9cae5ea9d19a7b831618ec
RoelerCoaster <RoelerCoaster@users.noreply.github.com>
2023-12-18 19:57:54 +0100

    Day 17 part 2 (still inefficient)

--- Solutions/Day17/Day17.cs
+++ Solutions/Day17/Day17.cs
@@ -11,24 +11,28 @@ internal class Day17 : DayBase
 
     public override bool UseTestInput => false;
 
-    protected override PartToRun PartsToRun => PartToRun.Part1;
+    protected override PartToRun PartsToRun => PartToRun.Part2;
 
     protected override async Task<string> SolvePart1(string input)
     {
         var grid = input.Lines().Select(l => l.Digits()).ToArray();
 
-        var result = MinHeatLoss(grid, new(0, 0), new(grid.Length - 1, grid[0].Length - 1), 3);
+        var result = MinHeatLoss(grid, new(0, 0), new(grid.Length - 1, grid[0].Length - 1), 0, 3);
 
         return result.ToString();
     }
 
     protected override async Task<string> SolvePart2(string input)
     {
-        throw new NotImplementedException();
+        var grid = input.Lines().Select(l => l.Digits()).ToArray();
+
+        var result = MinHeatLoss(grid, new(0, 0), new(grid.Length - 1, grid[0].Length - 1), 4, 10);
+
+        return result.ToString();
     }
 
     // Basicallly a modified A*
-    private int MinHeatLoss(int[][] grid, GridCoordinate start, GridCoordinate dest, int maxSteps)
+    private int MinHeatLoss(int[][] grid, GridCoordinate start, GridCoordinate dest, int minSteps, int maxSteps)
     {
         var crucibleStart = new CrucibleData(start, maxSteps, CardinalDirection.East);
         var openSet = new HashSet<CrucibleData> { crucibleStart };
@@ -55,14 +59,14 @@ internal class Day17 : DayBase
             var current = openSet.MinBy(c => cellData[c].FScore)!;
             var currentCellData = cellData[current];
 
-            if (new GridCoordinate(current.Row, current.Col) == dest)
+            if (new GridCoordinate(current.Row, current.Col) == dest && current.StepsRemaining <= maxSteps - minSteps)
             {
                 return currentCellData.GScore;
             }
 
             openSet.Remove(current);
 
-            foreach (var neighbor in GetNeighbors(current, maxSteps, grid.Length - 1, grid[0].Length - 1))
+            foreach (var neighbor in GetNeighbors(current, minSteps, maxSteps, grid.Length - 1, grid[0].Length - 1))
             {
                 var neighborCellData = cellData[neighbor];
                 var gScore = currentCellData.GScore + grid[neighbor.Row][neighbor.Col];
@@ -81,24 +85,30 @@ internal class Day17 : DayBase
         throw new Exception("No path found");
     }
 
-    private IEnumerable<CrucibleData> GetNeighbors(CrucibleData current, int maxSteps, int maxRow, int maxCol)
+    private IEnumerable<CrucibleData> GetNeighbors(CrucibleData current, int minSteps, int maxSteps, int maxRow, int maxCol)
     {
-        var potentialCoordinates = new List<CrucibleData>
+        var potentialNeighbors = new List<CrucibleData>
         {
-            new (CoordinateInDirection(current, current.Direction.RotateClockwise()), maxSteps - 1, current.Direction.RotateClockwise()),
-            new (CoordinateInDirection(current, current.Direction.RotateCounterClockwise()), maxSteps - 1, current.Direction.RotateCounterClockwise()),
             new (CoordinateInDirection(current, current.Direction), current.StepsRemaining - 1, current.Direction)
         };
 
-        foreach (var coordinate in potentialCoordinates)
+        if (current.StepsRemaining <= maxSteps - minSteps)
+        {
+            potentialNeighbors.AddRange([
+                new(CoordinateInDirection(current, current.Direction.RotateClockwise()), maxSteps - 1, current.Direction.RotateClockwise()),
+                new(CoordinateInDirection(current, current.Direction.RotateCounterClockwise()), maxSteps - 1, current.Direction.RotateCounterClockwise()),
+            ]);
+        }
+
+        foreach (var neighbor in potentialNeighbors)
         {
             // Coordinate must be inside the grid
-            if (coordinate.Row < 0 || coordinate.Col < 0 || coordinate.Row > maxRow || coordinate.Col > maxCol || coordinate.StepsRemaining < 0)
+            if (neighbor.Row < 0 || neighbor.Col < 0 || neighbor.Row > maxRow || neighbor.Col > maxCol || neighbor.StepsRemaining < 0)
             {
                 continue;
             }
 
-            yield return coordinate;
+            yield return neighbor;
 
         }
     }
