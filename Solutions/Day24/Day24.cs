a848cad099791bc59b2d46af16bb27d45eee9fb3
RoelerCoaster <RoelerCoaster@users.noreply.github.com>
2023-12-27 19:21:20 +0100

    Day 24 part 2 using Z3

--- Solutions/Day24/Day24.cs
+++ Solutions/Day24/Day24.cs
@@ -1,3 +1,4 @@
+using Microsoft.Z3;
 using RoelerCoaster.AdventOfCode.Year2023.Internals.Model;
 using RoelerCoaster.AdventOfCode.Year2023.Util;
 
@@ -9,7 +10,7 @@ internal class Day24 : DayBase
 
     public override bool UseTestInput => false;
 
-    protected override PartToRun PartsToRun => PartToRun.Part1;
+    protected override PartToRun PartsToRun => PartToRun.Both;
 
     protected override async Task<string> SolvePart1(string input)
     {
@@ -46,7 +47,10 @@ internal class Day24 : DayBase
 
     protected override async Task<string> SolvePart2(string input)
     {
-        throw new NotImplementedException();
+        var hailstones = input.Lines().Select(ParseHailstone).ToList();
+        var result = GetStoneParameters(hailstones);
+
+        return (result.X + result.Y + result.Z).ToString();
     }
 
     private HailStone ParseHailstone(string line)
@@ -90,12 +94,123 @@ internal class Day24 : DayBase
             / (denominator);
 
 
-        return new(x * 1_000_000,y * 1_000_000);
+        return new(x * 1_000_000, y * 1_000_000);
     }
 
-    public bool PointIsInTheFuture(HailStone hailstone, Point2D point)
+    private bool PointIsInTheFuture(HailStone hailstone, Point2D point)
     {
         return (point.X - hailstone.Start.X) / hailstone.Velocity.X >= 0;
+    }
+
+    private Point3D GetStoneParameters(List<HailStone> hailstones)
+    {
+        // We build a system of equations where for each hailstone i we add the equations
+        // x_i + v_x,i * t_i == x_stone + v_x,stone * t_i
+        // y_i + v_y,i * t_i == y_stone + v_y,stone * t_i
+        // y_i + v_y,i * t_i == y_stone + v_y,stone * t_i
+        // t_i >= 0
+        // 
+        // Solving this using z3 should give us (x_stone, y_stone, z_stone)
+        using var ctx = new Context(new Dictionary<string, string>() { { "model", "true" } });
+
+        var expressions = new List<BoolExpr>();
+
+        var xStone = (IntExpr)ctx.MkConst(ctx.MkSymbol("x_stone"), ctx.IntSort);
+        var yStone = (IntExpr)ctx.MkConst(ctx.MkSymbol("y_stone"), ctx.IntSort);
+        var zStone = (IntExpr)ctx.MkConst(ctx.MkSymbol("z_stone"), ctx.IntSort);
+        var vxStone = (IntExpr)ctx.MkConst(ctx.MkSymbol("vx_stone"), ctx.IntSort);
+        var vyStone = (IntExpr)ctx.MkConst(ctx.MkSymbol("vy_stone"), ctx.IntSort);
+        var vzStone = (IntExpr)ctx.MkConst(ctx.MkSymbol("vz_stone"), ctx.IntSort);
+
+        var counter = 1;
+        foreach (var hailstone in hailstones)
+        {
+            var t = (IntExpr)ctx.MkConst(ctx.MkSymbol($"t_{counter}"), ctx.IntSort);
+
+            // x_i + v_x,i* t_i == x_stone + v_x,stone* t_i
+            var xExpr = ctx.MkEq(
+                ctx.MkAdd(
+                    ctx.MkInt((long)hailstone.Start.X),
+                    ctx.MkMul(
+                        ctx.MkInt((long)hailstone.Velocity.X),
+                        t
+                    )
+                ),
+                ctx.MkAdd(
+                    xStone,
+                    ctx.MkMul(
+                        vxStone,
+                        t
+                    )
+                )
+            );
+
+            // y_i + v_y,i* t_i == y_stone + v_y,stone* t_i
+            var yExpr = ctx.MkEq(
+                ctx.MkAdd(
+                    ctx.MkInt((long)hailstone.Start.Y),
+                    ctx.MkMul(
+                        ctx.MkInt((long)hailstone.Velocity.Y),
+                        t
+                    )
+                ),
+                ctx.MkAdd(
+                    yStone,
+                    ctx.MkMul(
+                        vyStone,
+                        t
+                    )
+                )
+            );
+
+            // y_i + v_y,i * t_i == y_stone + v_y,stone * t_i
+            var zExpr = ctx.MkEq(
+                ctx.MkAdd(
+                    ctx.MkInt((long)hailstone.Start.Z),
+                    ctx.MkMul(
+                        ctx.MkInt((long)hailstone.Velocity.Z),
+                        t
+                    )
+                ),
+                ctx.MkAdd(
+                    zStone,
+                    ctx.MkMul(
+                        vzStone,
+                        t
+                    )
+                )
+            );
+
+            // t_i >= 0
+            var tExpr = ctx.MkGe(t, ctx.MkInt(0));
+
+            expressions.AddRange(new[]
+            {
+                xExpr, yExpr, zExpr, tExpr
+            });
+
+            counter++;
+        }
+
+
+        var solver = ctx.MkSolver();
+
+        solver.Assert(expressions.ToArray());
+
+        if (solver.Check() == Status.SATISFIABLE)
+        {
+            var model = solver.Model;
+
+            return new(
+                ((IntNum)model.Evaluate(xStone)).Int64,
+                ((IntNum)model.Evaluate(yStone)).Int64,
+                ((IntNum)model.Evaluate(zStone)).Int64
+            );
+        }
+        else
+        {
+            throw new Exception();
+        }
     }
 }
 
